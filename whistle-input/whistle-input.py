6c24d42a3b3c6de19cf955eade40a0cc6e8405be
Leonie Schrod <leonie.schrod@t-online.de>
2024-05-05 11:03:07 +0200

    (ugly) menu navigation

--- whistle-input/whistle-input.py
+++ whistle-input/whistle-input.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyaudio
 import pyglet
 import pynput
+import re
 
 """
 - detect and react to whistled frequency chirps in real time
@@ -14,17 +15,16 @@ for a downwards chirp."
 - generalize technique to other applications
     - pynput: trigger key presses (up/down) to nagivate in arbitrary GUI menus by whistling
 
-(3P) - upwards and downwards whistling are detected correctly and robustly
-(2P) - detection is robust against background noise
-(1P) - low latency between input and detection
-(1P) - the pyglet test program works
-(1P) - triggered key events work 
+- [/] (3P) - upwards and downwards whistling are detected correctly and robustly
+- [x] (2P) - detection is robust against background noise 
+- [ ] (1P) - low latency between input and detection
+- [ ] (1P) - the pyglet test program works
+- [ ] (1P) - triggered key events work 
 """
 
-# make window
-# make rect
-# get chirps
-# move rect up + down
+WIDTH = 500
+HEIGHT = 500 
+window = pyglet.window.Window(WIDTH, HEIGHT)
 
 # Set up audio stream
 # reduce chunk size and sampling rate for lower latency
@@ -35,63 +35,76 @@ CHANNELS = 1  # Mono audio
 RATE = 10000
 p = pyaudio.PyAudio()
 
-# print info about audio devices
-# let user select audio device
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
+class Stream:
+    devices = {}
+    stream:pyaudio.Stream = None
 
-for i in range(0, numdevices):
-    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    def set_device_info():
+        info = p.get_host_api_info_by_index(0)
+        num_devices = info.get('deviceCount')
 
-print('select audio device:')
-input_device = int(input())
+        for i in range(0, num_devices):
+            # via: audio-sample.py
+            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                d = p.get_device_info_by_host_api_device_index(0, i).get('name')
+                print(d)
+                Stream.devices[i] = d
 
-# open audio input stream
-stream = p.open(format=FORMAT,
+    def open_audio_stream(idx:int):
+        if idx in Stream.devices:
+            print(Stream.devices[idx])
+            Stream.has_settings = True
+            Stream.stream = Stream.get_stream(idx)
+            print("strema", Stream.stream)
+        else: print("No device with this id")
+
+    def get_stream(device_id:int) -> pyaudio.Stream:
+        stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK_SIZE,
-                input_device_index=input_device)
+                input_device_index=device_id)
+        return stream
+    
+    def get_input_frequency(): # see: karaoke.py
+        data = Stream.stream.read(CHUNK_SIZE)
 
+        # Convert audio data to numpy array
+        data = np.frombuffer(data, dtype=np.int16)
 
-def get_input_frequency(): # see: karaoke.py
-    data = stream.read(CHUNK_SIZE)
+        # from dsp.ipynb
+        data = data * np.hamming(len(data)) # ??
 
-    # Convert audio data to numpy array
-    data = np.frombuffer(data, dtype=np.int16)
+        fft_data = np.fft.rfft(data) # get only positive
+        peak = np.argmax(np.abs(fft_data)) # get the peak coeffiecients
 
-    # from dsp.ipynb
-    data = data * np.hamming(len(data)) # ??
+        freqs = np.fft.fftfreq(len(fft_data)) # get all frequencies
+        freq = freqs[peak] # find peak frequency
 
-    fft_data = np.fft.rfft(data) # get only positive
-    peak = np.argmax(np.abs(fft_data)) # get the peak coeffiecients
+        freq_in_hertz = abs(freq * RATE)
+        return round(freq_in_hertz)
 
-    freqs = np.fft.fftfreq(len(fft_data)) # get all frequencies
-    freq = freqs[peak] # find peak frequency
-
-    freq_in_hertz = abs(freq * RATE)
-    return round(freq_in_hertz)
 
 THRESHOLD = 150
-CHIRP_LEN = 2
+CHIRP_LEN = 3
 CHIRP_THRESHOLD = 500
 class Detector:
     prev_freq = 0 # default value
     chirps = []
     counter = 0
 
+    # TODO: make robuster
     # NOTE: chirp ↑ ~ 1969 to 3353 // chirp ↓ ~ 3372 to 1988 (diff off ~ 1000)
     def find_chirps():
-        freq = get_input_frequency()
+        freq = Stream.get_input_frequency()
 
         if Detector.prev_freq != 0:
             if freq > Detector.prev_freq + THRESHOLD or freq < Detector.prev_freq - THRESHOLD:
-                print("??", freq)
+                # print("??", freq)
                 if len(Detector.chirps) < CHIRP_LEN:
                     Detector.chirps.append(freq)
-                    print("++")
+                    # print("++")
 
                 elif len(Detector.chirps) >= CHIRP_LEN:
                     # compare first and last
@@ -99,10 +112,12 @@ class Detector:
                         diff = Detector.chirps[0] - freq
                         if diff > CHIRP_THRESHOLD:
                             print("↓", Detector.chirps, freq)
+                            Menu.navigate_menu(0)
                     elif Detector.chirps[0] < freq: # ↑
                         diff = freq - Detector.chirps[0]
                         if diff > CHIRP_THRESHOLD:
                             print("↑", Detector.chirps, freq) 
+                            Menu.navigate_menu(1)
                     print("...", Detector.chirps)
                     Detector.reset()
 
@@ -116,61 +131,97 @@ class Detector:
             Detector.prev_freq = freq
             print("i", freq)
 
-        # print("f", freq)
-
     def reset():
         Detector.counter = 0
         Detector.chirps = []
         Detector.prev_freq = 0
-
-        # if change in freq from previous is big enough apppend to chirps
-        # if len(chirps) > 10: compair first and last entry
-            # if 0 > n -> down
-            # if 0 < n -> up
-            # check if distance big enoug
-        # reset if too long without changes
-
-        # if Detector.prev_freq < freq - THRESHOLD:
-        #     print("d", freq)
-        #     Detector.prev_freq = freq
-        #     pass
-        # elif Detector.prev_freq > freq + THRESHOLD:
-        #     print("u", freq)
-        #     Detector.prev_freq = freq
-        # else: print("p", freq)
-
-        # Detector.prev_freq = freq
-        # print("f", freq)
-
-        # if  Detector.prev_freq > freq - THRESHOLD:
-        #     if len(Detector.downs) < 10:
-        #         Detector.downs.append(freq)
-        #     else:
-        #         if Detector.downs[0] > 
-        #         # check first and last entry
-        #     # print("d -> ", Chirp_Detector.prev_freq)
-        #     pass
-        # elif freq > Detector.prev_freq + THRESHOLD:
-        #     print("u -> ", Detector.prev_freq)
-        # else: print("n")
-        # Detector.prev_freq = freq
-        # PREV_FREQ = freq
-        # print(Chirp_Detector.prev_freq, freq)
     
     # get input_frequency
-    # when change from silence -> save freq as previous
-    # detect change in frequency -> difference needs to be above threshold (1000/2000?)
+    # when change from "silence" -> save freq as previous
+    # detect change in frequency -> difference needs to be above threshold
     # chirp = lower to higher or higher to lower
-    pass
 
-# continuously capture and plot audio singal
-while True:
-    # Read audio data from stream
-    data = stream.read(CHUNK_SIZE)
+class Menu:
+    """A dummy menu"""
+    width = 200
+    height = 50
+    length = 5
+    items = []
+    unselected = (221, 219, 219)
+    selected = (143, 252, 43)
+    selected_id = 3
 
-    # Convert audio data to numpy array
-    data = np.frombuffer(data, dtype=np.int16)
+    batch = pyglet.graphics.Batch()
 
-    # print(get_input_frequency())
-    Detector.find_chirps()
+    # def __init__(self) -> None:
+    #     self.batch = pyglet.graphics.Batch()
+    #     pass
 
+    def init_info():
+        pass
+
+    def init_menu():
+        for i in range(0, Menu.length+1):
+            Menu.create_menu_item(i)
+        # Menu.draw_menu()
+
+    def create_menu_item(idx):
+        x = Menu.width - 30
+        y = HEIGHT - (idx * (HEIGHT/Menu.length) ) + 30 # ↑
+
+        if idx == Menu.selected_id:
+            color = Menu.selected
+        else: color = Menu.unselected
+
+        rect = pyglet.shapes.Rectangle(x, y, Menu.width, Menu.height,
+                                       color, batch=Menu.batch)
+        Menu.items.append(rect)
+
+    def draw_menu():
+        Menu.batch.draw()
+
+    def navigate_menu(direction):
+        old = Menu.items[Menu.selected_id]
+        old.color = Menu.unselected
+
+        if direction == 0: # down
+            if Menu.selected_id < Menu.length:
+                Menu.selected_id += 1
+        elif direction == 1: # up
+            if Menu.selected_id > 0:
+                Menu.selected_id -= 1
+        
+        # change selection color
+        new = Menu.items[Menu.selected_id]
+        new.color = Menu.selected
+
+    pass 
+
+
+# ----- WINDOW INTERACTION ----- #
+@window.event
+def on_draw():
+    window.clear()
+
+    if Stream.stream != None:
+        Detector.find_chirps()
+        Menu.draw_menu()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.ESCAPE:
+        window.close()
+    key = pyglet.window.key.symbol_string(symbol)
+    if (re.match(r"_\d", key)):
+        num = key.split("_")[1]
+        Stream.open_audio_stream(int(num))
+
+# ----- RUN GAME ----- #
+if __name__ == '__main__':
+    # init some stuff
+    Stream.set_device_info()
+    Menu.init_menu()
+    # print("notes", notes.notes)
+    # run the game
+    pyglet.app.run()
